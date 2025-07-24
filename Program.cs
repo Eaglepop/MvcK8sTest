66ead568cdb74c1c359097cd84775163322537d6
ISCOM - EagleChang <eaglechang@iscom.com.tw>
2025-07-24 14:48:59 +0800

    restrict port in program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// 明確指定要監聽 80 port!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-builder.WebHost.ConfigureKestrel(serverOptions =>{serverOptions.ListenAnyIP(80); });
+// 強制 listen 在 80!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+builder.WebHost
+    .ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.ListenAnyIP(80);
+    })
+    .UseUrls("http://*:80"); //這行最關鍵，避免 ASPNETCORE_URLS 干擾
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
